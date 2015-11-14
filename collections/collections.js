22890d17cbfbaad0164e49d2e04d590084c0a5de
William Lin <wlin26@yahoo.com>
2015-11-14 16:38:15 -0500

    Restructured project

---
+++ collections/collections.js
@@ -0,0 +1,3 @@
+UserList = new Mongo.Collection("userList");
+
+DoctorList = new Mongo.Collection("docList");
--- router.js
+++ router.js
@@ -1,3 +1,5 @@
 Router.configure({layoutTemplate: 'layout'});
 
 Router.route('', {name: "welcome"});
+Router.route('/myhome', {name: "home"});
+Router.route('/schedule', {name: "schedule"});
