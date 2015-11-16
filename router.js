69b4b82c1a3ac1342116ca6b23257d87ce6b62e5
William Lin <wlin26@yahoo.com>
2015-11-16 12:07:14 -0500

    Added a treatment file with html and added a welcome page with welcome msg that use to be on the home page

--- router.js
+++ router.js
@@ -1,5 +1,7 @@
 Router.configure({layoutTemplate: 'layout'});
 
+Router.route('', {name: "welcome"});
 Router.route('/myhome', {name: "home"});
 Router.route('/calendar', {name: "calendar"});
 Router.route('/medicine', {name: "medicine"});
+Router.route('/treatment', {name: "treatment"});
