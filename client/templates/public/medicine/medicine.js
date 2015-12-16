c9ff1e03bb846af365074ab4b8a62943f54d8fd3
William Lin <wlin26@yahoo.com>
2015-12-16 11:50:32 -0500

    Made changes to meds page

--- client/templates/public/medicine/medicine.js
+++ client/templates/public/medicine/medicine.js
@@ -1,3 +1,22 @@
+Meds = new Mongo.Collection("meds");
+
 if(Meteor.isClient){
-	
+	Template.medicine.helpers({
+		meds: function(){
+			return Meds.find({});
+		}
+	});
+
+	Template.medicine.events({
+		"submit .addMed": function(event){
+			event.preventDefault();
+
+			var text = event.target.text.value;
+
+			Meds.insert({
+				text: text,
+				createdAt: new Date()
+			});
+		}
+	});
 }
