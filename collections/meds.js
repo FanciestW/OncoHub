6c6ae26d0a8c44539ae88b29ef3441fce4bcb860
William Lin <wlin26@yahoo.com>
2016-01-13 13:24:21 -0500

    Created SimpleSchema for Meds

--- collections/meds.js
+++ collections/meds.js
@@ -1,1 +1,18 @@
 Meds = new Mongo.Collection("Meds");
+
+MedsSchema = new SimpleSchema({
+
+	"medName": {
+		type: String,
+		label: "Name of Medicine",
+		optional: false
+	},
+
+	"medDose": {
+		type: Number,
+		label: "Medication dosage in mg",
+		optional: false
+	}
+});
+
+Meds.attachSchema(MedsSchema);
