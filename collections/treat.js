df29b1328226d4659886aa4fb835b8c2774915bc
William Lin <wlin26@yahoo.com>
2016-01-19 10:05:59 -0500

    Fixed error involving missing quotes around label

--- collections/treat.js
+++ collections/treat.js
@@ -4,20 +4,20 @@ Treaments.attachSchema(new SimpleSchema({
 
 	treatmentName: {
 		type: String,
-		label: Treatment Name,
+		label: "Treatment Name",
 		optional: false,
 		max: 250
 	},
 
 	treatmentDate: {
 		type: Date,
-		label: Date of Treament,
+		label: "Date of Treament",
 		optional: false,
 	},
 
 	treatmentDesc: {
 		type: String,
-		label: Description of Treatment,
+		label: "Description of Treatment",
 		optional: true,
 		max: 3000
 	}
