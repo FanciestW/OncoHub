eddf74d4fce9bfbfd7aa9c7300227bbc5219daf5
William Lin <wlin26@yahoo.com>
2016-01-19 09:48:44 -0500

    Added a collection with simpleschema for treatments, plan to add more fields for each treatment

---
+++ collections/treat.js
@@ -0,0 +1,25 @@
+Treatments = new Mongo.Collection("treatments");
+
+Treaments.attachSchema(new SimpleSchema({
+
+	treatmentName: {
+		type: String,
+		label: Treatment Name,
+		optional: false,
+		max: 250
+	},
+
+	treatmentDate: {
+		type: Date,
+		label: Date of Treament,
+		optional: false,
+	},
+
+	treatmentDesc: {
+		type: String,
+		label: Description of Treatment,
+		optional: true,
+		max: 3000
+	}
+
+}));
