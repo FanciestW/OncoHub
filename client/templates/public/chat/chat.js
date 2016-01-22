ae9c9a63b6141b46919b5c4ed5c30ed317c7a992
William Lin <wlin26@yahoo.com>
2016-01-22 12:47:33 -0500

    Added a functional chat feature

--- client/templates/public/chat/chat.js
+++ client/templates/public/chat/chat.js
@@ -2,9 +2,23 @@ if(Meteor.isClient){
 
 	Template.chat.helpers({
 		msg: function(){
-			return Chat.find().fetch().reverse();
+			return Chat.find();
 		}
-	});
+	});	
+
+
 
 	Meteor.subscribe("Chat");
 }
+$(document).ready(function(){
+
+	var height = 0;
+	$('#eachMsg').each(function(i, value){
+		height += parseInt($(this).height());
+	});
+
+	height += '';
+
+	$('#chatArea').animate({scrollTop: height});
+
+});
